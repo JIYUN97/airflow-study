8615f251c3f2a5d9dc14aba3f91638f217b61101
minil <matenduel@gmail.com>
2023-10-15 16:06:03 +0900

    FIX: Modify the docker-compose files

--- docker/compose/trigger.sh
+++ docker/compose/trigger.sh
@@ -1,9 +1,20 @@
-#docker network create airflow
+# create external network
+docker network create airflow
+docker network create ext_comp
+
+# create external volume
+docker volume create airflow
 docker volume create airflow_logs
 docker volume create airflow_dags
 docker volume create airflow_plugins
 
-docker compose --file ext_comp.yaml up -d
-docker compose --file server.yaml up -d
-docker compose --file web_server.yaml up -d
-docker compose --file worker.yaml up -d
+docker volume create airflow-study_postgres-db-volume
+
+# launch the project
+docker compose --project-name ext_comp --file ext_comp.yaml up -d
+docker compose --project-name server --file server.yaml up -d
+docker compose --project-name web_server --file web_server.yaml up -d
+docker compose --project-name worker --file worker.yaml up -d
+
+docker compose --project-name worker-ext --file worker2.yaml up -d
+
